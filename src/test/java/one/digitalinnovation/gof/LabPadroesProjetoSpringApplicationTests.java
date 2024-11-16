d2685e5113fa4211c4cc22a01d776cdf0b2f7993
Alex de Moraes <alexdemoraes@gmail.com>
2024-11-16 18:25:43 -0300

    Commit inicial

---
+++ src/test/java/one/digitalinnovation/gof/LabPadroesProjetoSpringApplicationTests.java
@@ -0,0 +1,14 @@
+package one.digitalinnovation.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class LabPadroesProjetoSpringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
